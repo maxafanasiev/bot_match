6bf6582c434e70034f5c3da071576d3b9b08714b
maxafanasiev <80445276+maxafanasiev@users.noreply.github.com>
2023-06-05 12:46:33 +0300

    Update Notebook.py

--- Notebook.py
+++ Notebook.py
@@ -3,29 +3,58 @@ from collections import UserDict
 
 
 class Note:
-    def __init__(self, note: str):
-        self.content = []
-        self.content.append(note)
+    def __init__(self, content: str):
+        self.content = ''
+        if len(content) <= 80:
+            self.content = content
+        else:
+            raise ValueError('Note too long.')
         self.creation_date = datetime.datetime.now()
 
-    def add_note(self, note: str):
-        if note not in self.content:
-            self.content.append(note)
+    def __eq__(self, other_note):
+        return self.content == other_note.content
 
-    # def __repr__(self):
-    #     return f"Note : {self.content} (Created on {self.creation_date})"
-
-    def __str__(self):
-        return ' ,'.join(self.content)
+    def __repr__(self):
+        return self.content
 
 
 class Notebook(UserDict):
     def __init__(self):
+        super().__init__()
         self.data = {}
+
+    def __setitem__(self, tags, note: Note):
+        tags.sort()
+        if tuple(tags) not in self.data.keys():
+            self.data[tuple(tags)] = []
+            self.data[tuple(tags)].append(note)
+        else:
+            self.data[tuple(tags)].append(note)
+
+
+    def __getitem__(self, tags):
+        matching_notes = []
+        for note_tags, notes in self.data.items():
+            if all(tag in note_tags for tag in tags):
+                if isinstance(notes, list):
+                    for note in notes:
+                        matching_notes.append(note)
+                else:
+                    matching_notes.append(notes)
+
+        return matching_notes
+
+    def search_in_content(self, search_string):
+        matching_notes = []
+        for note_list in self.data.values():
+            for note in note_list:
+                if search_string.lower() in note.content.lower():
+                    matching_notes.append(note)
+        return matching_notes
 
     def iterator(self, n=10):
         self.page = 0
-        self.record_per_page = int(n)
+        self.record_per_page = n
         self.out = list(self.data.items())
 
         while True:
@@ -46,65 +75,49 @@ class Notebook(UserDict):
         if self.keys():
             out += '-' * 77 + '\n'
             out += '| {:^20} | {:^50} |\n'.format('Tags', 'Note')
-            for page in self.iterator(2):
+            for page in self.iterator(1):
                 out += '-' * 77 + '\n'
-                out += ' {:^77} \n'.format(f"Page #{page_num}")
+                out += ' {:^77} \n'.format(f"Note #{page_num}")
                 out += '-' * 77 + '\n'
                 for record in page:
-                    out += '| {:^20} | {:^50} |\n'.format(', '.join(record[0]), str(record[1]))
+                    out += '| {:^20} | {:^50} |\n'.format(', '.join(record[0]),
+                                                          ', '.join([note.content for note in (record[1])]))
                 page_num += 1
         else:
             out += '| {:^77} |\n'.format('Notebook is empty.')
         out += '-' * 77 + '\n'
         return out
 
-    def add_note(self, tags, note):
-        self.data[tuple(tags)] = note
-
-    def __getitem__(self, tags):
-        matching_notes = []
-        for note_tags, note in self.data.items():
-            if all(tag in note_tags for tag in tags):
-                matching_notes.append(note)
-        return matching_notes
-
-    def search_note(self, search_string):
-        for note_obj in self.data.values():
-            for note in note_obj.content:
-                if search_string in note:
-                    return note_obj
-
 
 notebook = Notebook()
+
 # Додавання нотаток до словника
-notebook.add_note(['tag1', 'tag2'], Note('Content 1'))
-notebook.add_note(['tag2', 'tag3'], Note('Content 2'))
-notebook.add_note(['tag3', 'tag4'], Note('Ctent 3'))
+note1 = Note('Content 1')
+note2 = Note('Content 11ghjkdfhgkjdfhgkjhdfkghdfkj')
+note3 = Note('Content 2')
+note4 = Note('Ctent 3')
 
-notebook.search_note('Content 1').add_note('Cfdsfsdfds')
+notebook[['tag1', 'tag2']] = note1
+notebook[['tag2', 'tag1']] = note2
+notebook[['tag2', 'tag3']] = note3
+notebook[['tag3', 'tag4']] = note4
+#
+
+
+print('\n Друк пошуку за тегами')
+result = notebook[['tag2']]
+result1 = notebook[['tag3']]
+result2 = notebook[['tag4']]
+print(result)
+print(result1)
+print(result2)
+
+
+print('\nВивід пошуку за змістом')
+result_search = notebook.search_in_content('Content')
+print(result_search)
+
+
+
+print('\n Друк Notebook')
 print(notebook)
-# Отримання нотаток за тегами
-# result = notebook[['tag2']]
-# result1 = notebook[['tag3']]
-# result2 = notebook[['tag4']]
-
-# result_search = notebook.search_note('onte')
-
-# print('\n')
-#
-# for note in result:
-#     print(note)
-#
-# print('\n')
-#
-# for note in result1:
-#     print(note)
-#
-# print('\n')
-#
-# for note in result2:
-#     print(note)
-#
-# print('\n')
-
-# print(', '.join([note for note in result_search]))
